b8d4105999a4490c43fc4c4b1270157be265379a
ChenshuLiu <chenshuneuro@gmail.com>
2022-04-21 11:06:26 -0700

    added section of using regularization in r to improve network performance

--- DL source code.R
+++ DL source code.R
@@ -93,3 +93,120 @@ table(Predicted = pred,
       # NOTE: for confusion matrix, we are using the original y_test_actual, not encoded
       Actual = y_test_actual)
 ##########################################################################
+# Applying regularization to deal with overfitting
+library(keras)
+library(readr)
+library(tidyr)
+library(tibble)
+library(plotly)
+
+# specify the number of feature variables for the dataset to be downloaded
+num_words <- 5000
+imdb <- dataset_imdb(num_words = num_words)
+
+# train test split
+c(train_data, train_labels) %<-% imdb$train
+c(test_data, test_labels) %<-% imdb$test
+# training and test data are both stored as lists
+length(train_data)
+
+# multi-hot encoding
+multi_hot_sequences <- function(sequences, dimension){
+  multi_hot <- matrix(0, 
+                      # the number of samples in the sequences
+                      # sequences are stored as lists
+                      nrow = length(sequences), 
+                      ncol = dimension)
+  for(i in 1 : length(sequences)){
+    # sequences[[i]] extracts the label of the words in the text sample i
+    # which ever word is included in that sequence will be assigned 1 at row i
+    multi_hot[i, sequences[[i]]] <- 1
+  }
+  multi_hot
+}
+
+train_data <- multi_hot_sequences(train_data, num_words)
+test_data <- multi_hot_sequences(test_data, num_words)
+
+baseline_model <-
+  keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = num_words) %>%
+  layer_dense(units = 16, activation = "relu") %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+baseline_model %>% compile(
+  optimizer = "adam",
+  loss = "binary_crossentropy",
+  metrics = list("accuracy")
+)
+
+baseline_model %>% summary()
+
+baseline_history <- baseline_model %>% fit(
+  train_data,
+  train_labels,
+  epochs = 20,
+  # recall: batch size controls the number of training samples to work through before updating parameters
+  batch_size = 512,
+  validation_data = list(test_data, test_labels),
+  # get loss and accuracy reports
+  verbose = 2
+)
+
+plot(baseline_history)
+
+l2_model <-
+  keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = num_words,
+              # apply regularization in the layer_dense function's argument
+              kernel_regularizer = regularizer_l2(l = 0.001)) %>%
+  layer_dense(units = 16, activation = "relu",
+              kernel_regularizer = regularizer_l2(l = 0.001)) %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+l2_model %>% compile(
+  optimizer = "adam",
+  loss = "binary_crossentropy",
+  metrics = list("accuracy")
+)
+
+l2_model %>% summary()
+
+l2_history <- l2_model %>% fit(
+  train_data,
+  train_labels,
+  epoch = 20,
+  batch_size = 512,
+  validation_data = list(test_data, test_labels),
+  verbose = 2
+)
+
+plot(l2_history)
+
+drop_model <- keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = num_words) %>%
+  # a new layer to specify the dropout rate
+  layer_dropout(0.6) %>%
+  layer_dense(units = 16, activation = "relu") %>%
+  layer_dropout(0.6) %>%
+  layer_dense(units = 1, activation = "sigmoid")
+
+drop_model %>% compile(
+  optimizer = "adam",
+  loss = "binary_crossentropy",
+  metrics = list("accuracy")
+)
+
+drop_model %>% summary()
+
+drop_history <- drop_model %>% fit(
+  train_data,
+  train_labels,
+  epoch = 20,
+  batch_size = 512,
+  validation_data = list(test_data, test_labels),
+  verbose = 2
+)
+
+plot(drop_history)
+##########################################################################
